65b0f11761d1c876507d9aabac01deac90a1c83e
root <root@ip-172-31-93-174.ec2.internal>
2019-12-04 00:31:28 +0000

    test2367

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,8 @@
 FROM centos
+VOULME /data
+ENV TEST=4.5.0
 MAINTAINER devops <testxxxx@test.com>
 ADD ./resource /resource
-RUN /resource/chef.sh 
+RUN /resource/chef.sh && rm -rf /resource
 WORKDIR /data
 
--- resource/chef.sh
+++ resource/chef.sh
@@ -3,6 +3,6 @@
 yum update 
 cd /resource
 yum install -y wget 
-wget https://packages.chef.io/files/stable/chefdk/${TEST}/el/8/chefdk-${TEST}-1.el7.x86_64.rpm
-rpm -ivh chefdk-${TEST}-1.el7.x86_64.rpm
+wget https://packages.chef.io/files/stable/chefdk/$TEST/el/8/chefdk-$TEST-1.el7.x86_64.rpm
+rpm -ivh chefdk-$TEST-1.el7.x86_64.rpm
 
